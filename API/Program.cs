0e119064a957d4a7d8ad645a44c6c4b0f0929048
Rijad Alihodzic <alihodza77@gmail.com>
2024-08-04 02:55:33 +0200

    Arhitektura

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 
 var app = builder.Build();
 
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
