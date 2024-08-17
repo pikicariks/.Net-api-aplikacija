1e91fe3857d05125d22fd1a7fe43c2295882dac4
Rijad Alihodzic <alihodza77@gmail.com>
2024-08-17 16:14:36 +0200

    Registracija usera

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, inject, input, Input, Output, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  private accountService = inject(AccountService);
+
+cancelRegister = output<boolean>();
+model:any = {}
+
+register(){
+  this.accountService.register(this.model).subscribe({
+    next:Response=>{
+      console.log(Response);
+      this.cancel();
+    },
+    error:err=>console.log(err)
+  });
+}
+
+cancel(){
+  this.cancelRegister.emit(false);
+}
+}
